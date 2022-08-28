8609e08ee6e322183af9d99cf0ca0f2b732b830c
datamonsterr <phamthanhdat17092004@gmail.com>
2022-08-28 17:51:52 +0700

    fjsalfj

---
+++ SpamBot.cpp
@@ -0,0 +1,31 @@
+﻿#include "SpamBot.h"
+#include "Robot/Robot.h"
+#include "Robot/Global.h"
+
+ROBOT_NS_USE_ALL
+
+#define ROBOT_OS_WIN
+
+
+int main()
+{
+	Keyboard keyboard;
+	keyboard.AutoDelay.Min = 5;
+	keyboard.AutoDelay.Max = 10;
+
+	std::string text;
+	std::cin >> text;
+	std::string textModified;
+
+	for (unsigned int i = 0; i < text.length(); i++) {
+		if (std::isupper(text[i])) {
+			textModified += "+";
+		}
+		textModified += text[i];
+	}
+
+	Timer::Sleep(5000);
+	keyboard.Click(textModified.c_str());
+
+	return 0;
+}
